4a56788ebe8db7a5186d72b4e5b1c029062c16b0
RYeeshuDhurandhar <76108672+RYeeshuDhurandhar@users.noreply.github.com>
2022-04-20 04:31:38 +0530

    Add files via upload

---
+++ Q3_ikj.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+#define N 128
+
+long current_time()
+{
+    struct timespec time;
+    timespec_get(&time, CLOCK_MONOTONIC);
+    return time.tv_sec * 1000000000 + time.tv_nsec;
+}
+
+void multiply(int matrix1[N][N],
+              int matrix2[N][N],
+              int result[N][N])
+{
+    int i, j, k;
+    for (i = 0; i < N; i++)
+    {
+        for (k = 0; k < N; k++)
+        {
+            result[i][j] = 0;
+            for (j = 0; j < N; j++)
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+        }
+    }
+}
+
+void random_matrix(int matrix[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] = (rand() % 100) * 0.21;
+        }
+    }
+}
+
+int main()
+{
+    long start_programExT = current_time();
+    int i, j;
+    int result[N][N];
+    int matrix1[N][N];
+    int matrix2[N][N];
+    random_matrix(matrix1);
+    random_matrix(matrix2);
+
+    srand(time(0));
+
+    long start_time_meat_portion = current_time();
+
+    multiply(matrix1, matrix2, result);
+
+    long end_time_meat_portion = current_time();
+
+    cout << endl
+         << "Time taken for meat portion of program (in ns): " << end_time_meat_portion - start_time_meat_portion << endl;
+
+    long end_programExT = current_time();
+    cout << endl
+         << "Program Execution Time (in ns): " << end_programExT - start_programExT << endl;
+    return 0;
+}
---
+++ Q4_kij.py
@@ -0,0 +1,40 @@
+from audioop import mul
+import random
+import time
+
+
+def mul_mat(N):
+    rslt = [[0] * N] * N
+    mat1 = [[0] * N] * N
+    mat2 = [[0] * N] * N
+    serial = 0
+
+    for i in range(0, N):
+        for j in range(0, N):
+            mat1[i][j] = random.randint(0, 50)
+
+    for i in range(0, N):
+        for j in range(0, N):
+            mat2[i][j] = random.randint(0, 50)
+
+    start_meat_portion = time.time()
+    for k in range(0, N):
+        for i in range(0, N):
+            rslt[i][j] = 0
+
+            for j in range(0, N):
+                rslt[i][j] += mat1[i][k] * mat2[k][j]
+    end_meat_portion = time.time()
+    meat_portion_time = end_meat_portion - start_meat_portion
+    return meat_portion_time
+
+start_programExT = time.time()
+
+N = 128
+meat_portion_time = mul_mat(N)
+
+end_programExT = time.time()
+
+programExT = end_programExT - start_programExT
+print('Time taken for meat portion of program (in s): ', meat_portion_time)
+print('Program Execution Time (in s): ', programExT)
